6b17c09689b6336b3b33d48d1381c1f7820ce2a5
Daniel Nyong <danielnyong66@gmail.com>
2025-04-02 05:37:37 +0100

    Updated docker compose file

--- firebase-server-config.ts
+++ firebase-server-config.ts
@@ -1,28 +1,28 @@
-import { getApp, initializeApp } from "firebase-admin/app";
+import {getApp, initializeApp} from "firebase-admin/app";
 import fbAdmin from "firebase-admin";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import {getAuth} from "firebase-admin/auth";
+import {getFirestore} from "firebase-admin/firestore";
 
-const { credential } = fbAdmin;
+const {credential} = fbAdmin;
 let auth = null;
 let db = null;
 
 if (!process.env.DISABLE_FIREBASE) {
-  const appName = "linkID-print-qr";
-  if (!fbAdmin.apps.find((app) => app?.name === appName))
-    initializeApp(
-      {
-        credential: credential.cert({
-          projectId: process.env.FB_PROJECT_ID ?? "",
-          clientEmail: process.env.FB_CLIENT_EMAIL ?? "",
-          privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "",
-        }),
-      },
-      appName
-    );
-  auth = getAuth(getApp(appName));
-  db = getFirestore(getApp(appName));
+	const appName = "emergency-contact-demo-print-qr";
+	if (!fbAdmin.apps.find((app) => app?.name === appName))
+		initializeApp(
+			{
+				credential: credential.cert({
+					projectId: process.env.FB_PROJECT_ID ?? "",
+					clientEmail: process.env.FB_CLIENT_EMAIL ?? "",
+					privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "",
+				}),
+			},
+			appName
+		);
+	auth = getAuth(getApp(appName));
+	db = getFirestore(getApp(appName));
 }
 
-export { auth };
+export {auth};
 export default db;
