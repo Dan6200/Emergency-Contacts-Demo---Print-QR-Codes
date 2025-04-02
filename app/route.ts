e6184a821752f67a4d703f64bc7246e5e606d4b8
Daniel Nyong <danielnyong66@gmail.com>
2025-04-02 16:21:04 +0100

    testing

--- app/route.ts
+++ app/route.ts
@@ -17,10 +17,62 @@ const redis = new Redis({
 // You can convert the logo into base64 using an online tool and place the result here
 
 export const dynamic = "force-dynamic";
-export async function GET() {
-	const cacheKey = "residents-pdf"; // Define a cache key for the generated PDF
+async function generateResidentsPDF(): Promise<Buffer> {
+	const rooms = await getAllRooms().catch((e) => {
+		throw new Error("Failed to Retrieve Residents Data -- Tag:24.\n\t" + e);
+	});
 
-	// Check Redis for cached PDF
+	const doc = new jsPDF();
+
+	await Promise.all(
+		rooms.map(
+			async ({id, roomNo, address}: Residence & {id: string}, idx) => {
+				const qrCodeDataUri = await QRcode.toDataURL(
+					new URL(`/room/${id}/`, process.env.DOMAIN).toString()
+				);
+
+				doc.setFontSize(20);
+				doc.setFont("Helvetica", "bold");
+				doc.text("RESIDENT INFORMATION - SCAN TO REVEAL", 30, 90);
+				doc.setFont("Helvetica", "normal");
+
+				doc.setLineWidth(8);
+				doc.setDrawColor(255, 0, 0);
+				doc.rect(75, 100, 60, 60);
+				doc.addImage(qrCodeDataUri, "PNG", 75, 100, 60, 60);
+				doc.setFont("Helvetica", "bold");
+				doc.text("INSTANT ACCESS TO EMERGENCY INFO", 35, 183);
+
+				let street = address
+					.match(/^[A-Za-z ]+(?=\s\d)/gm)
+					?.join(" ")
+					.toUpperCase();
+
+				if (!street)
+					throw new Error(
+						"Please provide the street name to address: " + address
+					);
+				const streetRaw = street.split(" ");
+				const regex = /^(?!.*(ROAD|STREET|RD|ST|DRIVE|WAY)).+$/;
+				const streetName = streetRaw.filter((word) => regex.test(word));
+
+				doc.setFontSize(16);
+				doc.setFont("Helvetica", "normal");
+				doc.text(streetName.join(""), 75, 173);
+				doc.text("-", 112, 173);
+				doc.text("#" + roomNo, 120, 173);
+
+				if (idx < rooms.length - 1) doc.addPage();
+			}
+		)
+	);
+
+	return Buffer.from(doc.output("arraybuffer"));
+}
+
+export async function GET() {
+	const cacheKey = "residents-pdf";
+
 	try {
 		const cachedPDF = await redis.get(cacheKey);
 		if (cachedPDF) {
